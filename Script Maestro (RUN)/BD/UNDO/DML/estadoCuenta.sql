8cdad904e0ef1a86eb65b197ca82d887ed37668d
Jostin Cruz <79998491+javierf89@users.noreply.github.com>
2024-10-31 17:37:41 -0600

    CORRIGIENDO ERRORES 4

--- Script Maestro (RUN)/BD/UNDO/DML/estadoCuenta.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO estadoCuenta (id, tipo, motivo) VALUES ('1', 'A', 'Activo acorde al pago de su tipo de cuenta');
-INSERT INTO estadoCuenta (id, tipo, motivo) VALUES ('2', 'I', 'Inactivo acorde a falta de pago de su tipo de cuenta');
--- Script Maestro (RUN)/BD/UNDO/DML/formaPago.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO formaPago (id, tipo) VALUES ('1', 'Efectivo');
-INSERT INTO formaPago (id, tipo) VALUES ('2', 'Transferencia');
-INSERT INTO formaPago (id, tipo) VALUES ('3', 'Transferencia');
